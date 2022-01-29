730b953ccd3b5680eceacf81c62c4b0c6020897f
Cedola <101516727+Cedola@users.noreply.github.com>
2022-01-29 19:49:34 +0000

    Fix wrong path

--- src/main.c
+++ src/main.c
@@ -255,4 +255,4 @@ int Game()
 		PlayerPosition.y += PlayerMovement.y;
 
 		if ((Area.ceiling[RoundDown(PlayerPosition.y, 40) / 40][RoundDown(PlayerPosition.x - 100, 40) / 40] > 0) ||
-			(A
+			(Area.ceiling[RoundDow
