cc2810c20ee0cf33d43b56f710309e05b18ad1ae
Cedola <101516727+Cedola@users.noreply.github.com>
2022-02-26 18:00:30 +0000

    Prepare 0.26.0 release

--- src/main.c
+++ src/main.c
@@ -260,4 +260,4 @@ int Game()
 			PlayerPosition.y = PreviousPlayerPosition.y;
 		}
 
-		if ((PlayerPosition.x != PreviousPlayerPosition.x) || (PlayerPositi
+		if ((PlayerPosition.x != PreviousPlayerPosition.x) || (PlayerPosition.y != PreviousPlayerPosition.
