288e7e05711c2c0a027ee9bb851afdcf13923e67
Cedola <101516727+Cedola@users.noreply.github.com>
2022-06-26 11:19:43 +0000

    Add waffle.io badge

--- src/main.c
+++ src/main.c
@@ -278,4 +278,4 @@ int Game()
 		{
 			CharacterSprite.CurrentSprite.x = AttackAnimationWalking[1];
 		}
-		else if ((Tick - AttackStartTick < 8 && Attack
+		else if ((Tick - AttackStartTick < 8 && AttackAnimation == true) && (moving ==
