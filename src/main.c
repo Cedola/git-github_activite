e2ef9fcf30f1fd040ee21182dbc4ccef59551d09
Cedola <101516727+Cedola@users.noreply.github.com>
2022-06-26 16:11:09 +0000

    Fix migration.

--- src/main.c
+++ src/main.c
@@ -278,4 +278,4 @@ int Game()
 		{
 			CharacterSprite.CurrentSprite.x = AttackAnimationWalking[1];
 		}
-		else if ((Tick - AttackStartTick < 8 && AttackAnimation == true) && (moving == true && (CharacterS
+		else if ((Tick - AttackStartTick < 8 && AttackAnimation == true) && (moving == true && (CharacterSprite.CurrentSprite.y == 1 |
