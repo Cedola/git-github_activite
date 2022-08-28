e060c3b090905b21d38f3c40bee8885fd57c4d37
Cedola <101516727+Cedola@users.noreply.github.com>
2022-08-28 23:01:29 +0000

    Update components.md

--- src/main.c
+++ src/main.c
@@ -286,4 +286,6 @@ int Game()
 		{
 			CharacterSprite.CurrentSprite.x = AttackAnimationStanding[1];
 		}
-		else if ((Tick - AttackStartTick < 8 && AttackAnimation == true) && (moving == false && (CharacterSprite.CurrentSprite.y == 1 || CharacterSprite.CurrentS
+		else if ((Tick - AttackStartTick < 8 && AttackAnimation == true) && (moving == false && (CharacterSprite.CurrentSprite.y == 1 || CharacterSprite.CurrentSprite.y == 2)))
+		{
+			
