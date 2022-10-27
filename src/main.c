c3e37ef64f1ab27877ca1176eab0fc78831d20c2
Cedola <101516727+Cedola@users.noreply.github.com>
2022-10-27 15:59:53 +0000

    mobile version

--- src/main.c
+++ src/main.c
@@ -304,4 +304,4 @@ int Game()
 			}
 			else if ((Tick % 10 == 0) && (moving == true) && (CharacterSprite.CurrentSprite.y == 1 || CharacterSprite.CurrentSprite.y == 2))
 			{
-				CharacterSprite.CurrentSprite.x = PlayerAnimati
+				CharacterSprite.CurrentSprite.x = PlayerAnimation[CurrentPlayerA
