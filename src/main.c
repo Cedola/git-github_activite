0ba99241a8a86dc83f72cd473888d7d57d3e437b
Cedola <101516727+Cedola@users.noreply.github.com>
2022-12-21 07:31:25 +0000

    golint

--- src/main.c
+++ src/main.c
@@ -338,4 +338,4 @@ int Game()
 		if ((Area.ceiling[RoundDown(PlayerPosition.y, 40) / 40][RoundDown(PlayerPosition.x - 100, 40)/ 40] > 0) ||
 			(Area.ceiling[RoundDown(PlayerPosition.y, 40) / 40][RoundUp(PlayerPosition.x - 100, 40)/ 40] > 0))
 		{
-			PlayerPosition = Previo
+			PlayerPosition = PreviousPlayerPosition;
